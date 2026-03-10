cc77e7bbe2fb78a217858eea0de7161dfedf42d1
DavidRamirezDeLaParra <david.ramirezd@campusucc.edu.co>
2026-03-10 18:40:20 -0500

    config: establecer URL y llave anonima real de Supabase

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,9 +1,11 @@
 // lib/core/utils/constants.dart
 
 class AppConstants {
-  // Reemplaza estas comillas con tus datos reales de Supabase (Settings > API)
-  static const String supabaseUrl = 'https://tu_proyecto.supabase.co';
-  static const String supabaseAnonKey = 'tu_llave_anon_aqui';
+  // Tu Project URL real
+  static const String supabaseUrl = 'https://phozwxriubhwbkdqvfkz.supabase.co';
+  
+  // Tu API Key (anon public) real
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBob3p3eHJpdWJod2JrZHF2Zmt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxODM3NTksImV4cCI6MjA4ODc1OTc1OX0.XFm9bW5Olwt3XrP0z17u9o9phZpBVOu0UajDVNqQTsw';
 
   static const String appName = 'Spider-Sense';
 }
