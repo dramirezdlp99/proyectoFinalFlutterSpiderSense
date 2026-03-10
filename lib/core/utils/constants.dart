28f82acb3af256b5d2273b8bcb00994f8f7745f4
DavidRamirezDeLaParra <david.ramirezd@campusucc.edu.co>
2026-03-10 18:17:07 -0500

    estructura: crear carpetas del proyecto y constantes de supabase

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,9 @@
+// lib/core/utils/constants.dart
+
+class AppConstants {
+  // Reemplaza estas comillas con tus datos reales de Supabase (Settings > API)
+  static const String supabaseUrl = 'https://tu_proyecto.supabase.co';
+  static const String supabaseAnonKey = 'tu_llave_anon_aqui';
+
+  static const String appName = 'Spider-Sense';
+}
