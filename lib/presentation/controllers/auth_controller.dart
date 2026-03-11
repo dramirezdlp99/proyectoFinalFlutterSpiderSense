3b17b61fa35d13a389c95a9cbba5881ec123ee86
DavidRamirezDeLaParra <david.ramirezd@campusucc.edu.co>
2026-03-11 14:58:55 -0500

    feat: añadir lógica de registro e internacionalización al controlador de autenticación

--- lib/presentation/controllers/auth_controller.dart
+++ lib/presentation/controllers/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -5,13 +6,37 @@ class AuthController extends GetxController {
   final _client = Supabase.instance.client;
   var isLoading = false.obs;
 
+  // Login Logic
   Future<void> login(String email, String password) async {
     try {
       isLoading.value = true;
       await _client.auth.signInWithPassword(email: email, password: password);
-      Get.snackbar('Éxito', 'Bienvenido a Spider-Sense');
+      Get.snackbar('login_success_title'.tr, 'login_success_msg'.tr,
+          snackPosition: SnackPosition.BOTTOM);
+      // Get.offAllNamed('/home'); // Redirección a Home cuando esté lista
     } catch (e) {
-      Get.snackbar('Error', 'No se pudo iniciar sesión');
+      Get.snackbar('error_title'.tr, 'login_error_msg'.tr,
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red.withOpacity(0.7),
+          colorText: Colors.white);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  // Register Logic
+  Future<void> signUp(String email, String password) async {
+    try {
+      isLoading.value = true;
+      await _client.auth.signUp(email: email, password: password);
+      Get.snackbar('reg_success_title'.tr, 'reg_success_msg'.tr,
+          snackPosition: SnackPosition.BOTTOM);
+      Get.toNamed('/login');
+    } catch (e) {
+      Get.snackbar('error_title'.tr, 'reg_error_msg'.tr,
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red.withOpacity(0.7),
+          colorText: Colors.white);
     } finally {
       isLoading.value = false;
     }
