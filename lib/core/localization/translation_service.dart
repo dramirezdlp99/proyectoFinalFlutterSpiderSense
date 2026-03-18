6e8a9f6bf0c56fd46f3e9585add22f6bab93ae4f
DavidRamirezDeLaParra <david.ramirezd@campusucc.edu.co>
2026-03-18 08:43:50 -0500

    mover TranslationService a su archivo propio sin duplicados

--- lib/core/localization/translation_service.dart
+++ lib/core/localization/translation_service.dart
@@ -1,20 +1,26 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class TranslationService extends Translations {
   static Map<String, Map<String, String>> translations = {};
 
-  // Carga los archivos JSON de la carpeta assets
   static Future<void> init() async {
     translations['en_US'] = await _loadJson('en-US');
     translations['es_ES'] = await _loadJson('es-ES');
   }
 
   static Future<Map<String, String>> _loadJson(String code) async {
-    final String response = await rootBundle.loadString('assets/langs/$code.json');
-    final Map<String, dynamic> data = json.decode(response);
-    return data.map((key, value) => MapEntry(key, value.toString()));
+    try {
+      final String response =
+          await rootBundle.loadString('assets/langs/$code.json');
+      final Map<String, dynamic> data = json.decode(response);
+      return data.map((key, value) => MapEntry(key, value.toString()));
+    } catch (e) {
+      debugPrint("Error loading translation $code: $e");
+      return {};
+    }
   }
 
   @override
