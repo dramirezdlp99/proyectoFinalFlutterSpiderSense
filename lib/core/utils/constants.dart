d22156d3ccdaa922f71fb144d75ae235ebc12960
DavidRamirezDeLaParra <david.ramirezd@campusucc.edu.co>
2026-04-02 17:38:43 -0500

    migrar constantes a variables de entorno con flutter_dotenv

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,11 +1,12 @@
-// lib/core/utils/constants.dart
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
-  // Tu Project URL real
-  static const String supabaseUrl = 'https://phozwxriubhwbkdqvfkz.supabase.co';
-  
-  // Tu API Key (anon public) real
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBob3p3eHJpdWJod2JrZHF2Zmt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxODM3NTksImV4cCI6MjA4ODc1OTc1OX0.XFm9bW5Olwt3XrP0z17u9o9phZpBVOu0UajDVNqQTsw';
+  static String get supabaseUrl =>
+      dotenv.env['SUPABASE_URL'] ?? '';
 
-  static const String appName = 'Spider-Sense';
+  static String get supabaseAnonKey =>
+      dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+
+  static String get appName =>
+      dotenv.env['APP_NAME'] ?? 'Spider-Sense';
 }
